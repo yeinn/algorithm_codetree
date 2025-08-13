d284f2985a4e871bcd0f6c41ccbd56ebc7ab134b
Gomguma <54708042+yeinn@users.noreply.github.com>
2025-08-13 23:24:05 +0900

    Title: 나눗셈의 나머지, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250813/나눗셈의 나머지/remainder-of-division.js
+++ 250813/나눗셈의 나머지/remainder-of-division.js
@@ -4,7 +4,7 @@ let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 const cnt = new Array(b).fill(0)
 
 //기수 변환 방식 집계
-while(a > 0){
+while(a > 1){
     const r = Math.floor(a % b)
    cnt[r]++
     a =  Math.floor(a / b)
